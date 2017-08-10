9b512f2a7e297b1e40d5478826aaa2bf2b0f8acf
1939612669@qq.com <as19910222>
2017-08-11 00:26:36 +0800

    测试

---
+++ spring/src/main/java/com/spring/MessageService.java
@@ -0,0 +1,8 @@
+package com.spring;
+
+/**
+ * Created by alibeibei on 2017/8/10.
+ */
+public interface MessageService {
+    String getMessage();
+}
---
+++ spring/src/main/java/com/spring/controller/HelloController.java
@@ -0,0 +1,8 @@
+package com.spring.controller;
+
+/**
+ * Created by alibeibei on 2017/8/10.
+ */
+public class HelloController {
+
+}
